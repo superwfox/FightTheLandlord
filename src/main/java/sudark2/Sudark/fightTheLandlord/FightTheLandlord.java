60aaf0aee3cf19aff1d275a1438bad7cd520a741
SUDARK <2963502563@qq.com>
2025-05-12 20:54:18 +0800

    Sudark

---
+++ src/main/java/sudark2/Sudark/fightTheLandlord/FightTheLandlord.java
@@ -0,0 +1,16 @@
+package sudark2.Sudark.fightTheLandlord;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FightTheLandlord extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
